ec690e2d961f0d77c98dc45ef0b88c2971e1118b
Vivek Vinze <vivek.vinze@gmail.com>
2020-06-01 20:31:40 +0530

    h

---
+++ index.js
@@ -0,0 +1,64 @@
+
+//Detecting mouse click
+var n = document.querySelectorAll(".drum").length;
+for(var i=0;i<n;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        var pressedKey = this.innerHTML;
+        makeSound(pressedKey);
+        buttonAnimation(pressedKey);
+    });
+}
+
+//Detecting keyboard press
+document.addEventListener("keypress", function (){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+
+function makeSound(key){
+    switch (key) {
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+            break;
+        default:
+
+    }
+}
+
+function buttonAnimation(key){
+    var activeButton = document.querySelector("."+key);
+
+    activeButton.classList.add("pressed");
+
+    function removeAnimation(){
+        activeButton.classList.remove("pressed");
+    }
+
+    setTimeout(removeAnimation,100);
+}
